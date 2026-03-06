6a3f75707a8031ded86cc78cddfedf7cc4911f26
SukhvirSingh1 <veer83594@gmail.com>
2026-03-06 14:30:40 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-#  test
-from  game_engine import GameEngine
-from database import Database
-
-db = Database()
-engine = GameEngine(db)
-# Simulate completing a task with ID 1
-engine.complete_task(1)
